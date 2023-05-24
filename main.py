32073af1e73c1011f5a316012ccad3eb0feb8fc4
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-24 17:53:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,25 @@
 import requests
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+import plotly.graph_objects as go
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def get_token_data():
-    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-    headers = {
-        "X-CMC_PRO_API_KEY": "0a244b8b-3664-4305-842e-d0a8b8027e4a"
-    }
-    params = {
-        "id": "18450",
-        "convert_id": "2781",
-        "aux": "num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,total_supply,market_cap_by_total_supply,volume_24h_reported,volume_7d,volume_7d_reported,volume_30d,volume_30d_reported,is_active,is_fiat"
-    }
-    
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-    
-    if response.status_code == 200 and "data" in data:
-        return data["data"]["18450"]
-    else:
-        return None
+    # Your implementation here
+    pass
 
-def get_chart_image_url():
-    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-    headers = {
-        "X-CMC_PRO_API_KEY": "0a244b8b-3664-4305-842e-d0a8b8027e4a"
-    }
-    params = {
-        "id": "18450",
-        "convert_id": "2781",
-        "aux": "chart",
-        "interval": "1d"
-    }
-    
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-    
-    if response.status_code == 200 and "data" in data:
-        return data["data"]["18450"]["chart"]["1d"]
-    else:
-        return None
+def generate_chart():
+    # Retrieve historical price data
+    # ...
+
+    # Generate chart using Plotly
+    fig = go.Figure(data=go.Scatter(x=dates, y=prices))
+    fig.update_layout(title='Price Chart', xaxis_title='Date', yaxis_title='Price')
+
+    # Save the chart as an image or display it
+    # ...
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Welcome to the Token Price Bot! Use /price to get token information.")
-
-def price(update: Update, context: CallbackContext):
-    token_data = get_token_data()
-    chart_image_url = get_chart_image_url()
-
-def price(update: Update, context: CallbackContext):
-    token_data = get_token_data()
-
-    import plotly.graph_objects as go
-
-def generate_chart():
-    # Retrieve historical price data
-    # ...
-
-    # Generate chart using Plotly
-    fig = go.Figure(data=go.Scatter(x=dates, y=prices))
-    fig.update_layout(title='Price Chart', xaxis_title='Date', yaxis_title='Price')
-
-    # Save the chart as an image or display it
-    # ...
-
-import plotly.graph_objects as go
-
-def generate_chart():
-    # Retrieve historical price data
-    # ...
-
-    # Generate chart using Plotly
-    fig = go.Figure(data=go.Scatter(x=dates, y=prices))
-    fig.update_layout(title='Price Chart', xaxis_title='Date', yaxis_title='Price')
-
-    # Save the chart as an image or display it
-    # ...
 
 def price(update: Update, context: CallbackContext):
     token_data = get_token_data()
@@ -98,13 +42,11 @@ def price(update: Update, context: CallbackContext):
 
         # Send the chart image along with the response
         with open('chart.png', 'rb') as chart_image:
-            update.message.reply_photo(chart_image)
-        
-        update.message.reply_text(response, parse_mode='HTML')
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=chart_image)
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=response, parse_mode='HTML')
     else:
-        update.message.reply_text("Unable to fetch token data.")
-
-
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Unable to fetch token data.")
 
 def main():
     bot_token = "6229379290:AAFZ5WTSqW-H1jGlpsrsZFQRPS9JITHPsS0"
