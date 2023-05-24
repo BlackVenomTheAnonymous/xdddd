81c8ff31ef244680a62031a15e4c6e02dfa53509
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-24 17:56:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import requests
-import plotly.graph_objects as go
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def get_token_data():
-    # Your implementation here
-    pass
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    headers = {
+        "X-CMC_PRO_API_KEY": "0a244b8b-3664-4305-842e-d0a8b8027e4a"
+    }
+    params = {
+        "id": "18450",
+        "convert_id": "2781",
+        "aux": "num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,total_supply,market_cap_by_total_supply,volume_24h_reported,volume_7d,volume_7d_reported,volume_30d,volume_30d_reported,is_active,is_fiat"
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    
+    if response.status_code == 200 and "data" in data:
+        return data["data"]["18450"]
+    else:
+        return None
 
-def generate_chart():
-    # Retrieve historical price data
-    # ...
-
-    # Generate chart using Plotly
-    fig = go.Figure(data=go.Scatter(x=dates, y=prices))
-    fig.update_layout(title='Price Chart', xaxis_title='Date', yaxis_title='Price')
-
-    # Save the chart as an image or display it
-    # ...
+def get_chart_image_url():
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    headers = {
+        "X-CMC_PRO_API_KEY": "0a244b8b-3664-4305-842e-d0a8b8027e4a"
+    }
+    params = {
+        "id": "18450",
+        "convert_id": "2781",
+        "aux": "chart",
+        "interval": "1d"
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    
+    if response.status_code == 200 and "data" in data:
+        return data["data"]["18450"]["chart"]["1d"]
+    else:
+        return None
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Welcome to the Token Price Bot! Use /price to get token information.")
+
+def price(update: Update, context: CallbackContext):
+    token_data = get_token_data()
+    chart_image_url = get_chart_image_url()
 
 def price(update: Update, context: CallbackContext):
     token_data = get_token_data()
@@ -38,15 +65,24 @@ def price(update: Update, context: CallbackContext):
         response += f"🚀 All-time High: {all_time_high}\n\n"
         response += "Paragen is a chain agnostic launchpad and incubator native to the BSC network. Its goal is to offer an extremely fair tiered system with guaranteed allocations focused on gaming and metaverse projects. 🎮🌌\n"
 
-        generate_chart()  # Generate the chart
+        keyboard = [
+            [
+                InlineKeyboardButton("Paragen - 🌐", url="https://t.me/paragenio"),
+                InlineKeyboardButton("Marketing - 💼", url="https://t.me/BullionDOT"),
+                InlineKeyboardButton("Buy - 💰", url="https://pancakeswap.finance/swap?outputCurrency=0x25382fb31e4b22e0ea09cb0761863df5ad97ed72"),
+            ],
+            [
+                InlineKeyboardButton("Website - 🌐", url="https://paragen.io/"),
+                InlineKeyboardButton("Project - 📋", url="https://paragen.io/projects"),
+                InlineKeyboardButton("CoinMarketCap - 💎", url="https://coinmarketcap.com/dexscan/bsc/0x447ff4dd9cee7f751cf3eb253dbb3c227747b31c/"),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
 
-        # Send the chart image along with the response
-        with open('chart.png', 'rb') as chart_image:
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=chart_image)
-
-        context.bot.send_message(chat_id=update.effective_chat.id, text=response, parse_mode='HTML')
+        update.message.reply_text(response, parse_mode='HTML', reply_markup=reply_markup)
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Unable to fetch token data.")
+        update.message.reply_text("Unable to fetch token data.")
+
 
 def main():
     bot_token = "6229379290:AAFZ5WTSqW-H1jGlpsrsZFQRPS9JITHPsS0"
